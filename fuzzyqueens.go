9b8b4c7155caeb9bef2250e607198fc8fb561d5b
Zellyn Hunter <zellyn@squareup.com>
2017-04-14 09:55:17 -0400

    unroll loop again, add run transcript

--- fuzzyqueens.go
+++ fuzzyqueens.go
@@ -6,6 +6,15 @@
 // rm -rf workdir/ && \
 // go-fuzz-build github.com/zellyn/fuzzyqueens \
 // && go-fuzz -bin=./fuzzyqueens-fuzz.zip -workdir=workdir
+//
+//
+// $ rm -rf workdir/ && go-fuzz-build github.com/zellyn/fuzzyqueens && go-fuzz -bin=./fuzzyqueens-fuzz.zip -workdir=workdir
+// 2017/04/14 09:52:46 slaves: 8, corpus: 30 (1s ago), crashers: 2, restarts: 1/0, execs: 0 (0/sec), cover: 0, uptime: 3s
+// 2017/04/14 09:52:49 slaves: 8, corpus: 30 (4s ago), crashers: 2, restarts: 1/6014, execs: 30072 (5005/sec), cover: 143, uptime: 6s
+// ^C2017/04/14 09:52:49 shutting down...
+// $ for i in workdir/crashers/*.output; do head -1 $i; done
+// panic: (0,5), (1, 2), (2, 4), (3, 7), (4, 0), (5, 3), (6, 1), (7, 6)
+// panic: (0,5), (1, 2), (2, 4), (3, 6), (4, 0), (5, 3), (6, 1), (7, 7)
 
 package fuzzyqueens
 
@@ -15,16 +24,6 @@ type pos struct {
 	y    int
 	sum  int
 	diff int
-}
-
-func lonely(queens []pos) bool {
-	l := len(queens) - 1
-	for i := 0; i < l; i++ {
-		if queens[l].y == queens[i].y || queens[l].sum == queens[i].sum || queens[l].diff == queens[i].diff {
-			return false
-		}
-	}
-	return true
 }
 
 func Fuzz(data []byte) int {
@@ -41,40 +40,115 @@ func Fuzz(data []byte) int {
 	var queens [8]pos
 	for x, d := range data {
 		y := int(d)
-		queens[x] = pos{
-			y:    y,
-			sum:  x + y,
-			diff: x - y,
-		}
+		queens[x] = pos{y: y, sum: x + y, diff: x - y}
 	}
 
-	if !lonely(queens[:2]) {
-		return 0
-	}
-	if !lonely(queens[:3]) {
-		return 0
-	}
-	if !lonely(queens[:4]) {
-		return 0
-	}
-	if !lonely(queens[:5]) {
-		return 0
-	}
-	if !lonely(queens[:6]) {
-		return 0
-	}
-	if !lonely(queens[:7]) {
-		return 0
-	}
-	if !lonely(queens[:8]) {
+	if connected(queens) {
 		return 0
 	}
 
-	// Eight women -- perhaps the only eight -- in the same space, yet
-	// cut off from each other, unable to communicate or lend
-	// support. That's a tragedy, not a solution.
+	// Eight women -- perhaps the only eight -- occupy the same space,
+	// yet are cut off from each other, unable to communicate or lend
+	// support. Panic.
 	panic(fmt.Sprintf("(%d,%d), (%d, %d), (%d, %d), (%d, %d), (%d, %d), (%d, %d), (%d, %d), (%d, %d)",
 		0, queens[0].y, 1, queens[1].y, 2, queens[2].y, 3, queens[3].y,
 		4, queens[4].y, 5, queens[5].y, 6, queens[6].y, 7, queens[7].y,
 	))
+}
+
+func connected(queens [8]pos) bool {
+	// Alas, no kind of un-unrolled loops work as well.
+
+	if queens[1].y == queens[0].y || queens[1].sum == queens[0].sum || queens[1].diff == queens[0].diff {
+		return true
+	}
+
+	if queens[2].y == queens[0].y || queens[2].sum == queens[0].sum || queens[2].diff == queens[0].diff {
+		return true
+	}
+	if queens[2].y == queens[1].y || queens[2].sum == queens[1].sum || queens[2].diff == queens[1].diff {
+		return true
+	}
+
+	if queens[3].y == queens[0].y || queens[3].sum == queens[0].sum || queens[3].diff == queens[0].diff {
+		return true
+	}
+	if queens[3].y == queens[1].y || queens[3].sum == queens[1].sum || queens[3].diff == queens[1].diff {
+		return true
+	}
+	if queens[3].y == queens[2].y || queens[3].sum == queens[2].sum || queens[3].diff == queens[2].diff {
+		return true
+	}
+
+	if queens[4].y == queens[0].y || queens[4].sum == queens[0].sum || queens[4].diff == queens[0].diff {
+		return true
+	}
+	if queens[4].y == queens[1].y || queens[4].sum == queens[1].sum || queens[4].diff == queens[1].diff {
+		return true
+	}
+	if queens[4].y == queens[2].y || queens[4].sum == queens[2].sum || queens[4].diff == queens[2].diff {
+		return true
+	}
+	if queens[4].y == queens[3].y || queens[4].sum == queens[3].sum || queens[4].diff == queens[3].diff {
+		return true
+	}
+
+	if queens[5].y == queens[0].y || queens[5].sum == queens[0].sum || queens[5].diff == queens[0].diff {
+		return true
+	}
+	if queens[5].y == queens[1].y || queens[5].sum == queens[1].sum || queens[5].diff == queens[1].diff {
+		return true
+	}
+	if queens[5].y == queens[2].y || queens[5].sum == queens[2].sum || queens[5].diff == queens[2].diff {
+		return true
+	}
+	if queens[5].y == queens[3].y || queens[5].sum == queens[3].sum || queens[5].diff == queens[3].diff {
+		return true
+	}
+	if queens[5].y == queens[4].y || queens[5].sum == queens[4].sum || queens[5].diff == queens[4].diff {
+		return true
+	}
+
+	if queens[6].y == queens[0].y || queens[6].sum == queens[0].sum || queens[6].diff == queens[0].diff {
+		return true
+	}
+	if queens[6].y == queens[1].y || queens[6].sum == queens[1].sum || queens[6].diff == queens[1].diff {
+		return true
+	}
+	if queens[6].y == queens[2].y || queens[6].sum == queens[2].sum || queens[6].diff == queens[2].diff {
+		return true
+	}
+	if queens[6].y == queens[3].y || queens[6].sum == queens[3].sum || queens[6].diff == queens[3].diff {
+		return true
+	}
+	if queens[6].y == queens[4].y || queens[6].sum == queens[4].sum || queens[6].diff == queens[4].diff {
+		return true
+	}
+	if queens[6].y == queens[5].y || queens[6].sum == queens[5].sum || queens[6].diff == queens[5].diff {
+		return true
+	}
+
+	if queens[7].y == queens[0].y || queens[7].sum == queens[0].sum || queens[7].diff == queens[0].diff {
+		return true
+	}
+	if queens[7].y == queens[1].y || queens[7].sum == queens[1].sum || queens[7].diff == queens[1].diff {
+		return true
+	}
+	if queens[7].y == queens[2].y || queens[7].sum == queens[2].sum || queens[7].diff == queens[2].diff {
+		return true
+	}
+	if queens[7].y == queens[3].y || queens[7].sum == queens[3].sum || queens[7].diff == queens[3].diff {
+		return true
+	}
+	if queens[7].y == queens[4].y || queens[7].sum == queens[4].sum || queens[7].diff == queens[4].diff {
+		return true
+	}
+	if queens[7].y == queens[5].y || queens[7].sum == queens[5].sum || queens[7].diff == queens[5].diff {
+		return true
+	}
+	if queens[7].y == queens[6].y || queens[7].sum == queens[6].sum || queens[7].diff == queens[6].diff {
+		return true
+	}
+
+	return false
 }
